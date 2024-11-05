88ade4ff94a0d8a09df8b74db5a8212513090c9b
Alejandro Lopez <alejandro.lopez@uabc.edu.mx>
2024-11-04 16:02:24 -0800

    Update Notification.java

--- src/android/Notification.java
+++ src/android/Notification.java
@@ -590,11 +590,4 @@ public class Notification extends CordovaPlugin {
             }
         }, 200);
     }
-       public static void setMargins (View v, int l, int t, int r, int b) {
-              if (v.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
-                      ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
-                      p.setMargins(l, t, r, b);
-                      v.requestLayout();
-              }
-       }
 }
