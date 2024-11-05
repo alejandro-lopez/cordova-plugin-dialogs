9e69929056c92d4300fcc642bf2c6c63ce5cad11
Alejandro Lopez <alejandro.lopez@uabc.edu.mx>
2024-11-04 16:23:52 -0800

    Update Notification.java

--- src/android/Notification.java
+++ src/android/Notification.java
@@ -43,6 +43,7 @@ import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.GridLayout.LayoutParams;
 import android.widget.LinearLayout;
 import androidx.core.text.HtmlCompat;
 import androidx.core.content.ContextCompat;
